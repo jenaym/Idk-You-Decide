df73fc40831ddc6d377c25519773fc3d236e4f0f
JAngel <43564263+jenaym@users.noreply.github.com>
2018-11-30 23:03:36 -0800

    final

--- original.js
+++ original.js
@@ -4,79 +4,30 @@ $('#suggestions-map').hide();
  var googleAPI_key = "AIzaSyC7vnbVasWSJ-2i10phXjk_Q07yE6tOa-M";
  var mapsURL = "https://maps.googleapis.com/maps/api/js?key=" + googleAPI_key;
 
-let coordinatesArray = [];
-// function initMap(myLatLng) {
-function initMap(coordinatesArray) {
-    //var myLatLng = {lat: -25.363, lng: 131.044};
-  
-    var map = new google.maps.Map(document.getElementById('map-container-5'), {
-      zoom: 12,
-      center: coordinatesArray[0]
-    });
 
-     
-        for (var i = 0; i < coordinatesArray.length; i++) {
-        var marker = new google.maps.Marker({
-          position: coordinatesArray[i],
-          map: map,
-          title: 'Hello World!'
-        });
-    
-      }
-};
-
-// initMap();
-
-var mapOptions = {
-  zoom: 4,
-  center: {lat: 47.6553216, lng: -122.3155712}
-}
-var map = new google.maps.Map(document.getElementById("map-container-5"), mapOptions);
-
-function createMarker() {
-    var marker = new google.maps.Marker({
-    position: myLatlng,
-    title:"Hello World!"
-});
-
-};
-
-// To add the marker to the map, call setMap();
-// marker.setMap(map);
-
-
-
-// $("#mapView").click(function initMap() {
-//     var map;
-//     var infowindow;
-
-//     //default setting to be in Seattle
-//     pos = {
-//         lat: 47.6553216,
-//         lng: -122.3155712
-//     };
-
-//     map = new google.maps.Map(document.getElementById('map-container-5'), {
-//         center: pos,
-//         zoom: 15
-//     });
-
-
-
+let locations = [];
 
 
 $("#search").click(function () {
     event.preventDefault();
     $("#suggestion-list").empty();
     $("#suggestion-map").empty();
-    $("#invalidSubject").empty();
+    $("#invalidLocation").empty();
+    locations = [];
 
 
 
      //a valid, fully-formed foursquare API request that includes these parameters 
      //looks like this:
      let subject = $('#activityTypeInput').val().toLowerCase();
-     let locationInput = $('#locationInput').val().trim() + ' ';
+     let locationInput = $('#locationInput').val().trim();
+     console.log(locationInput)
+     if ($('#locationInput').val().trim() + 'none' === 'none') {
+         console.log('no')
+     $('#invalidLocation').text('Please input a valid location.')
+       }
+
+
      console.log(locationInput);
      let limit = $('#numOfOptionsInput').val();
      let timeInput = $('#gotimeInput').val();
@@ -93,20 +44,19 @@ $("#search").click(function () {
         console.log(day);
     
 
-    //  if (dayInput === )
-     let priceInput = $('#priceInput').val();
-     //let location = "Seattle, WA";
+        let priceInput = "&price=" + $('#priceInput').val();
+        if (priceInput === '&price=No Price Preference') {
+          priceInput = '';
+        }
 
-
-     //let numOfPeople = $('#numOfPeopleInput').val().trim() + ' ';
 
      var clientID = "UWNQG4U3L43EJK21FJS5FWEYSXD3GCFZIJCFHBWWNN3QVCLY";
      var client_secret = "VQWGC1GO2UFFVXVYI2MYWUFLNW2AM3CQADXRCJ21NGSNUJRV";
 
     var queryURL = 'https://api.foursquare.com/v2/venues/explore' + "?client_id=" + clientID +
          "&client_secret=" + client_secret + "&near=" + locationInput + "&section=" + subject + "&time=any" +
-         "&day=" + day + "&price=" + priceInput + "&v=20181129";
-     //location needs to be in longitude and latitude
+         "&day=" + day + priceInput + "&v=20181129";
+
      console.log(queryURL);
 
      $.ajax({
@@ -121,54 +71,100 @@ $("#search").click(function () {
     
       for (let i = 0; i < limit; i++) {
         let listItem = $('<div class="suggestion">');
-        let options =  $('<span>').text(i + 1 + ' ');
-        let type = $('<span>').text(results[i].venue.name);
 
-        let venueID = results[i].venue.id;
+        let name = results[i].venue.name;
+        let type = $('<span>').text(name);
 
 
             let itemAddress = $("<div class='address'>");
             //itemAddress.text(JSON.stringify(results[i].venue.location.formattedAddress));
             let address = results[i].venue.location.formattedAddress;
-            itemAddress.text(address[0] + " " + address[1]);
+            vAddress = address[0] + " " + address[1];
+            itemAddress.text(vAddress);
 
-            let myLatLng = {
-                lat: results[i].venue.location.lat,
-                lng: results[i].venue.location.lng
-            }
-
-            coordinatesArray.push(myLatLng);
-
-            
-            // let lat =  results[i].venue.location.lat;
-            // let lng = results[i].venue.location.lng;
-
-            //createMarker(myLatLng);
-            // var myLatlng = new google.maps.LatLng(lat,lng);
-            // marker.setMap(map);
-            
-            
+            let description = results[i].venue.categories[0].name;
+        
             let itemDescription = $("<div class='description'>");
-            itemDescription.text("Activity Description: " + JSON.stringify(results[i].venue.categories[0].name));
+            itemDescription.text("Activity Description: " + description);
+
+            let lat = results[i].venue.location.lat;
+            let lng = results[i].venue.location.lng;
+
+            let placeLocation = [
+                name,
+                lat,
+                lng,
+                vAddress,
+                description
+            ];
+
+            locations.push(placeLocation);
             
-            // listItem.append(itemLink, dislike, like)
+
             listItem.append(type, itemAddress, itemDescription);
             $('#suggestions-list').append(listItem)
             $('.suggestion').addClass('list-group-item list-group-item-action list-group-item') 
 
-            
+                
+            //this puts option to choose from in the database
+            database.ref(dataCounter + '/').set({
+                name: name,
+                llat: lat,
+                llng: lng,
+                address: vAddress,
+                description: description,
+                });
 
+            
+            //this create a different name for each item in th data-base
+                dataCounter ++
+
+                database.ref('num/').set({
+                    dataCount: dataCounter
+                    });
 
         };
 
-        initMap(coordinatesArray);
     });
 });
+
+
+//Google Maps
+$("#mapView").click(function () { 
+
+var map = new google.maps.Map(document.getElementById('map-container-5'), {
+    zoom: 12,
+    center: new google.maps.LatLng(47.6553216,-122.3155712),
+
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  var infowindow = new google.maps.InfoWindow();
+
+  var marker, i;
+
+  for (i = 0; i < locations.length; i++) { 
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+      map: map
+    });
+
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+      return function() {
+
+        infowindow.setContent(locations[i][0] + '<br>' + 'Address: ' + locations[i][3] + '<br>' + 'Type: ' + locations[i][4]);
+        infowindow.open(map, marker);
+      }
+    })(marker, i));
+  }
+});
+
 
  
  $("#clear").click(function () {
     $('#suggestions-list').empty();
      $('#suggestions-map').empty();
+     locations = [];
   }); 
 
 $('#mapView').click (function () {
@@ -182,43 +178,4 @@ $('#mapView').click (function () {
   }); 
 
         
-
-         //var activityDiv = $("<div class = 'activity'>");
-
-         //using the foursquare API to find recommended places using
-         //users' input parameters
-
-         // storing the location of recommended spot
-         //var location = response.location;
-
-         // storing rating of the place
-
-         //storing 
   
-
-    //  //Weather API
-    //  var weatherAPI_key = "c4fb90d5728ed1563993c1a62caaf631";
-
-    //  var weatherURL = "https://api.openweathermap.org/data/2.5/weather? + q=" + location + "&appid=" + weatherAPI_key;
-
-    //  $.ajax({
-    //      url: weatherURL,
-    //      method: "GET"
-    //  }).then(function (response) {
-    //      var results = response.data;
-
-    //  });
-
-    //  //Google API 
-    //  var googleAPI_key = "AIzaSyC7vnbVasWSJ-2i10phXjk_Q07yE6tOa-M";
-
-    //  var mapsURL = "https://maps.googleapis.com/maps/api/js?key=" + googleAPI_key + "&callback=initMap"
-
-    //  $.ajax({
-    //      url: mapsURL,
-    //      method: "GET"
-    //  }).then(function (response) {
-    //      var results = response.data;
-
-    //  });
-
